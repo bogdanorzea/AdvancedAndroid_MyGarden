552ff03f6db819224016cd112edafd9f8f72574c
Asser <asser@udacity.com>
2017-03-21 14:44:02 +0200

    TWID.02-Solution-AddWateringService
    (cherry picked from commit a6dbb2c)

--- app/src/main/java/com/example/android/mygarden/PlantWateringService.java
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -1,10 +1,27 @@
 package com.example.android.mygarden;
 
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import android.app.IntentService;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.util.Log;
+import android.provider.Settings;
 
 import com.example.android.mygarden.provider.PlantContract;
 import com.example.android.mygarden.utils.PlantUtils;
@@ -12,22 +29,47 @@ import com.example.android.mygarden.utils.PlantUtils;
 import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
 import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
 
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
 public class PlantWateringService extends IntentService {
-    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden/waterplants";
+
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
 
     public PlantWateringService() {
-        super("com.example.android.mygarden/waterplants");
+        super("PlantWateringService");
     }
 
+    /**
+     * Starts this service to perform WaterPlants action with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
+    /**
+     * @param intent
+     */
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (null != intent){
-            if(ACTION_WATER_PLANTS.equals(intent.getAction())){
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) {
                 handleActionWaterPlants();
             }
         }
     }
 
+    /**
+     * Handle action WaterPlant in the provided background thread with the provided
+     * parameters.
+     */
     private void handleActionWaterPlants() {
         Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
         ContentValues contentValues = new ContentValues();
